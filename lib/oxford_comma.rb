fea9805c37b616cdf7c387d16f72ad8fd6443f0d
PJ Frias <peejfrias@gmail.com>
2018-01-04 00:54:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
 def oxford_comma(array)
-  last = array.last
-  array.pop
-  start = array.join(", ")
-  start << ", and #{last}"
-  start
+  if (array.size == 1)
+    array.join
+  elsif(array.size == 2)
+    array.join(" and ")
+  else
+    last = array.last
+    array.pop
+    start = array.join(", ")
+    start << ", and #{last}"
+    start
+  end
 end
